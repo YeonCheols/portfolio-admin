aeaa70dd8933e4e44d9eaaeb43b100c135af76c4
SungStack <s9292909@gmail.com>
2025-05-19 11:13:13 +0900

    feat: axios post instance 생성

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,3 +8,12 @@ export async function getData(url: string, params?: Record<string, unknown>) {
     return { status: false, error };
   }
 }
+
+export async function postData(url: string, data: Record<string, any>) {
+  try {
+    const response = await axiosInstance.post(url, data);
+    return response.data;
+  } catch (error) {
+    return { status: false, error };
+  }
+}
