5c933b16289945e0ae577ac718e5ca1c62cd7fb6
SungStack <s9292909@gmail.com>
2025-06-24 12:47:48 +0900

    feat: 페이지 이동 ProgressBar 추가

--- src/components/ui/app/layout.tsx
+++ src/components/ui/app/layout.tsx
@@ -1,13 +1,17 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { type ReactElement } from 'react';
 import { SideNav } from '../../nav';
 import AppContainer from './container';
+
+const ProgressBar = dynamic(() => import('@/components/ui/progressbar'), { ssr: false });
 
 export default function AppLayout({ children, isLogin }: { children: ReactElement; isLogin: boolean }) {
   if (!isLogin) {
     return (
       <AppContainer>
+        <ProgressBar />
         <div className="flex-grow overflow-auto">{children}</div>
       </AppContainer>
     );
@@ -16,6 +20,7 @@ export default function AppLayout({ children, isLogin }: { children: ReactElemen
   return (
     <>
       <AppContainer>
+        <ProgressBar />
         <SideNav />
         <div className="flex-grow overflow-auto">{children}</div>
       </AppContainer>
