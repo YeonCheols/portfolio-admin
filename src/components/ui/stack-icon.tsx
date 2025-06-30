93c7fb2d4f6c97f8eb3ce2b19e2affa720755a17
SungStack <s9292909@gmail.com>
2025-06-30 10:39:56 +0900

    fix(lint): console error waring 변경

--- src/components/ui/stack-icon.tsx
+++ src/components/ui/stack-icon.tsx
@@ -2,25 +2,25 @@ import { type JSX, useEffect, useState } from 'react';
 import { type ComponentType } from 'react';
 
 // 정적으로 아이콘들을 import
-import * as SiIcons from 'react-icons/si';
-import * as BsIcons from 'react-icons/bs';
-import * as FaIcons from 'react-icons/fa';
-import * as MdIcons from 'react-icons/md';
-import * as HiIcons from 'react-icons/hi';
-import * as FiIcons from 'react-icons/fi';
-import * as LuIcons from 'react-icons/lu';
-import * as TbIcons from 'react-icons/tb';
-import * as IoIcons from 'react-icons/io';
 import * as AiIcons from 'react-icons/ai';
-import * as GiIcons from 'react-icons/gi';
-import * as WiIcons from 'react-icons/wi';
-import * as DiIcons from 'react-icons/di';
 import * as BiIcons from 'react-icons/bi';
+import * as BsIcons from 'react-icons/bs';
 import * as CgIcons from 'react-icons/cg';
+import * as DiIcons from 'react-icons/di';
+import * as FaIcons from 'react-icons/fa';
+import * as FiIcons from 'react-icons/fi';
+import * as GiIcons from 'react-icons/gi';
 import * as GrIcons from 'react-icons/gr';
-import * as RxIcons from 'react-icons/rx';
+import * as HiIcons from 'react-icons/hi';
+import * as IoIcons from 'react-icons/io';
+import * as LuIcons from 'react-icons/lu';
+import * as MdIcons from 'react-icons/md';
 import * as RiIcons from 'react-icons/ri';
+import * as RxIcons from 'react-icons/rx';
+import * as SiIcons from 'react-icons/si';
+import * as TbIcons from 'react-icons/tb';
 import * as VscIcons from 'react-icons/vsc';
+import * as WiIcons from 'react-icons/wi';
 
 // 스택 메타데이터 타입
 interface StackMetadata {
