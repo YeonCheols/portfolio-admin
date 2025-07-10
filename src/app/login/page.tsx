b423f7ec5c3183807a094fce53ce1f9bfed44c58
SungStack <s9292909@gmail.com>
2025-07-10 12:58:47 +0900

    fix: 로컬환경 인증서 오류 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { LoginForm } from '@/components/ui/login-form';
 
-export default async function Login({ searchParams }: { searchParams: Promise<any> }) {
+export default async function Login({ searchParams }: { searchParams: Promise<{ callbackUrl?: string }> }) {
   const params = await searchParams;
   const callbackUrl = params.callbackUrl || '/';
 
